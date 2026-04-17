95066fa2f11b2d3e65cb36e368c20f6e22be5adf
Dat Mai <hiendat04@gmail.com>
2026-04-17 18:44:28 -0500

    feat: add loans microservices with CRUD operations and error handling

--- src/main/java/com/microservice/loans/LoansApplication.java
+++ src/main/java/com/microservice/loans/LoansApplication.java
@@ -1,9 +1,36 @@
 package com.microservice.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API documentation",
+				description = "Loans microservice REST API documentation for managing customer loans",
+				version = "1.0",
+				contact = @Contact(
+						name = "Dat Mai",
+						email = "hiendat04@gmail.com",
+						url = "https://github.com/hiendat04"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Loans microservice REST API documentation",
+				url = "https://www.datmai.com/swagger-ui.html"
+		)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
