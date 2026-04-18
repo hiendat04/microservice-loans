bd650b9ccde35d00da6babf3490557b6f2f04c85
Dat Mai <hiendat04@gmail.com>
2026-04-18 14:47:07 -0500

    fix: correct formatting in LoansRepository and update pom.xml for Java version and build configuration

--- src/main/java/com/microservice/loans/repository/LoansRepository.java
+++ src/main/java/com/microservice/loans/repository/LoansRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface LoansRepository extends JpaRepository<Loans, Long> {
+public interface  LoansRepository extends JpaRepository<Loans, Long> {
 
     Optional<Loans> findByMobileNumber(String mobileNumber);
 
